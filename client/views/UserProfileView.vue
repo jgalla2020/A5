63a5034fa1bcbaab928aeb7d80df0d76a80823b2
Jabes Gallardo <jgalla24@mit.edu>
2024-10-17 18:55:37 -0400

    Added Updating Profile to control flow

--- client/views/UserProfileView.vue
+++ client/views/UserProfileView.vue
@@ -1,11 +1,12 @@
 <script setup lang="ts">
 import ProfileComponent from "@/components/Profile/ProfileComponent.vue";
 import CreateProfileForm from "@/components/Profile/CreateProfileForm.vue";
+import EditProfileForm from "@/components/Profile/EditProfileForm.vue";
 
 import { useUserStore } from "@/stores/user";
 import { storeToRefs } from "pinia";
 
-const { hasProfile } = storeToRefs(useUserStore());
+const { hasProfile, updatingProfile } = storeToRefs(useUserStore());
 </script>
 
 <template>
@@ -13,6 +14,9 @@ const { hasProfile } = storeToRefs(useUserStore());
     <h1>User Profile</h1>
     <section v-if="hasProfile">
       <ProfileComponent />
+    </section>
+    <section v-else-if="updatingProfile">
+      <EditProfileForm />
     </section>
     <section v-else>
       <CreateProfileForm />
