25713fd193fa7fd7d2c140a1970e3a67dc8a8426
Jabes Gallardo <jgalla24@mit.edu>
2024-10-17 11:55:25 -0400

    Added a User Profile view

---
+++ client/views/UserProfileView.vue
@@ -0,0 +1,20 @@
+<script>
+import { useUserStore } from "@/stores/user";
+import { storeToRefs } from "pinia";
+</script>
+
+<template>
+  <main>
+    <h1>Profile Page</h1>
+    <section>
+      <h1></h1>
+      <h1></h1>
+    </section>
+  </main>
+</template>
+
+<style scoped>
+h1 {
+  text-align: center;
+}
+</style>
